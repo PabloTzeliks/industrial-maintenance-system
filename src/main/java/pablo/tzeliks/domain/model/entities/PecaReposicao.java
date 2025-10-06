3be7964b6442fa3a31173637be9219b5205bf386
PabloTzeliks <arq.pabloo@gmail.com>
2025-10-06 20:29:21 -0300

    PRT-004 feat: implementa entidade de Peça de Reposição

--- src/main/java/pablo/tzeliks/domain/model/entities/PecaReposicao.java
+++ src/main/java/pablo/tzeliks/domain/model/entities/PecaReposicao.java
@@ -1,4 +1,38 @@
 package pablo.tzeliks.domain.model.entities;
 
 public class PecaReposicao {
+
+    long id;
+    String nome;
+    double estoque;
+
+    public PecaReposicao(long id, String nome, double estoque) {
+        this.id = id;
+        this.nome = nome;
+        this.estoque = estoque;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(double estoque) {
+        this.estoque = estoque;
+    }
 }

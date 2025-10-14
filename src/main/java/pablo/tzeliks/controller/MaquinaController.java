43cca41b102467fcfc2842fd3f26d09e35f37b98
PabloTzeliks <arq.pabloo@gmail.com>
2025-10-14 18:02:43 -0300

    feat: adiciona MaquinaDAO com método de verificação caso máquina pode ser criada ou não

--- src/main/java/pablo/tzeliks/controller/MaquinaController.java
+++ src/main/java/pablo/tzeliks/controller/MaquinaController.java
@@ -1,4 +1,14 @@
 package pablo.tzeliks.controller;
 
+import pablo.tzeliks.dao.MaquinaDAO;
+import pablo.tzeliks.model.Maquina;
+
 public class MaquinaController {
+
+    private final MaquinaDAO maquinaDAO = new MaquinaDAO();
+
+    public void salvar(Maquina maquina) {
+
+        if ()
+    }
 }
--- src/main/java/pablo/tzeliks/dao/connection/Conexao.java
+++ src/main/java/pablo/tzeliks/dao/connection/Conexao.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class conexao {
+public class Conexao {
 
     private static final String URL = "jdbc:mysql://localhost:3306/ims_db?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";

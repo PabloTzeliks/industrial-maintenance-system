e7d20fbb3c51338ed518d8d2b3119c1f7ecc7e90
PabloTzeliks <arq.pabloo@gmail.com>
2025-10-14 18:02:43 -0300

    feat: adiciona MaquinaDAO com método de verificação caso máquina pode ser criada ou não

--- src/main/java/pablo/tzeliks/dao/MaquinaDAO.java
+++ src/main/java/pablo/tzeliks/dao/MaquinaDAO.java
@@ -1,4 +1,41 @@
 package pablo.tzeliks.dao;
 
+import pablo.tzeliks.dao.connection.Conexao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class MaquinaDAO {
+
+    public boolean existeMaquina(String setor, String nome) {
+
+        String sql = """
+                SELECT COUNT(*) Maquina WHERE setor = ? AND nome = ?;
+                """;
+
+        try (Connection conn = Conexao.getConexao();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, setor);
+            stmt.setString(2, nome);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+
+                int count = rs.getInt(1);
+
+                return count > 0;
+            }
+
+        } catch (SQLException e) {
+
+            System.err.println("Ocorreu um erro ao verificar se a Máquina é elegível ao cadastrar, observe: " + e.getMessage());
+            return false;
+        }
+
+        return false;
+    }
 }

1377c458cff1dedfb86a96fb9b95c84fbcc6932a
PabloTzeliks <arq.pabloo@gmail.com>
2025-10-14 16:11:15 -0300

    feat: implementa model de técnico

--- src/main/java/pablo/tzeliks/model/Tecnico.java
+++ src/main/java/pablo/tzeliks/model/Tecnico.java
@@ -1,4 +1,47 @@
 package pablo.tzeliks.model;
 
 public class Tecnico {
+
+    private long id;
+    private String nome;
+    private double estoque;
+
+    public Tecnico(long id, String nome, double estoque) {
+        this.id = id;
+        this.nome = nome;
+        this.estoque = estoque;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(double estoque) {
+        this.estoque = estoque;
+    }
+
+    @Override
+    public String toString() {
+        return "Tecnico{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", estoque=" + estoque +
+                '}';
+    }
 }
